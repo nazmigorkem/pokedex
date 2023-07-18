17a320c0387280eef78fc999474ab11adac04534
nazmigorkem <nazmigorkem@hotmail.com>
2023-07-18 13:52:52 +0300

    tested methods

--- java_ignite_2nd_day/src/Shapes/Main.java
+++ java_ignite_2nd_day/src/Shapes/Main.java
@@ -2,6 +2,9 @@ package Shapes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Rectangle(3, 4, "Red"));
+        Rectangle rectangle = new Rectangle(3, 4, "Red");
+        System.out.println(rectangle);
+        rectangle.setColor("Blue");
+        System.out.println(rectangle);
     }
 }
