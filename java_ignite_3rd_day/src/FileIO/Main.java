122e1424ce270e79820d4bcd39740a5d16d3c452
nazmigorkem <nazmigorkem@hotmail.com>
2023-07-19 16:47:58 +0300

    added object stream file operations

--- java_ignite_3rd_day/src/FileIO/Main.java
+++ java_ignite_3rd_day/src/FileIO/Main.java
@@ -2,14 +2,18 @@ package FileIO;
 
 public class Main {
     public static void main(String[] args) {
-        InputStreamFileOperations inputStreamFileOperations = new InputStreamFileOperations();
-        inputStreamFileOperations.writeFile("./test.txt", "test text.");
-        String inputStreamData = inputStreamFileOperations.read("./test.txt");
-        System.out.println(inputStreamData);
+//        InputStreamFileOperations inputStreamFileOperations = new InputStreamFileOperations();
+//        inputStreamFileOperations.writeFile("./test.txt", "test text.");
+//        String inputStreamData = inputStreamFileOperations.read("./test.txt");
+//        System.out.println(inputStreamData);
+//
+//        BufferedStreamFileOperations bufferedStreamFileOperations = new BufferedStreamFileOperations();
+//        bufferedStreamFileOperations.writeFile("./test.txt", "test text.");
+//        String bufferedInputStreamData = bufferedStreamFileOperations.read("./test.txt");
+//        System.out.println(bufferedInputStreamData);
 
-        BufferedStreamFileOperations bufferedStreamFileOperations = new BufferedStreamFileOperations();
-        bufferedStreamFileOperations.writeFile("./test.txt", "test text.");
-        String bufferedInputStreamData = bufferedStreamFileOperations.read("./test.txt");
-        System.out.println(bufferedInputStreamData);
+        ObjectStreamFileOperations objectStreamFileOperations = new ObjectStreamFileOperations();
+        objectStreamFileOperations.serialize(new Student(1, "testName", "testDepartment", 123, "testAddress"), "./test.txt");
+        objectStreamFileOperations.deserialize("./test.txt");
     }
 }
