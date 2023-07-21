d6b24134893b73155dd1ded85ab7df8c27f437f1
nazmigorkem <nazmigorkem@hotmail.com>
2023-07-21 11:16:19 +0300

    refactored generics

--- java_ignite_5th_day/src/Shop/Main.java
+++ java_ignite_5th_day/src/Shop/Main.java
@@ -2,7 +2,7 @@ package Shop;
 
 public class Main {
     public static void main(String[] args) {
-        ShoppingCart shoppingCart = new ShoppingCart();
+        ShoppingCart<Item> shoppingCart = new ShoppingCart<>();
         Water water = new Water(2.435, "Erikli");
         Water water2 = new Water(2.8, "Sirma");
 
--- java_ignite_5th_day/src/Shop/ShoppingCart.java
+++ java_ignite_5th_day/src/Shop/ShoppingCart.java
@@ -3,9 +3,9 @@ package Shop;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShoppingCart {
-    private final List<Item> items = new ArrayList<>();
-    public <T extends Item> void addItem(T item) {
+public class ShoppingCart <T extends Item> {
+    private final List<T> items = new ArrayList<>();
+    public void addItem(T item) {
         items.add(item);
     }
 
@@ -17,7 +17,7 @@ public class ShoppingCart {
         items.forEach(x -> System.out.println(x.toString()));
     }
 
-    public <T extends Item> T compareItems(T item1, T item2) {
+    public T compareItems(T item1, T item2) {
         return Double.compare(item1.getPrice(), item2.getPrice()) == 1 ? item1 : item2;
     }
 
