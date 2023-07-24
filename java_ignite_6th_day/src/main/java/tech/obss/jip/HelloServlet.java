8b34b90a9caaccb78cc7d3ccfbddd58c23ef333e
nazmigorkem <nazmigorkem@hotmail.com>
2023-07-24 16:15:23 +0300

    added java code to jsp file

--- java_ignite_6th_day/src/main/java/tech/obss/jip/HelloServlet.java
+++ java_ignite_6th_day/src/main/java/tech/obss/jip/HelloServlet.java
@@ -3,6 +3,8 @@ package tech.obss.jip;
 import java.io.*;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -12,15 +14,20 @@ public class HelloServlet extends HttpServlet {
     public void init() {
     }
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("This is get method");
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.getWriter().println("This is get method");
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("This is post method.");
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("This is post method.");
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        res.getWriter().println("This is service method.");
+        super.service(req, res);
     }
 
     public void destroy() {
