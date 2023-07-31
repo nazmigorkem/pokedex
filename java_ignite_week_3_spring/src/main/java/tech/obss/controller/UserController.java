4e30b9e7f94ff24ce40358de752e154972198d54
nazmigorkem <nazmigorkem@hotmail.com>
2023-07-31 15:53:33 +0300

    added filter and interceptor

--- java_ignite_week_3_spring/src/main/java/tech/obss/controller/UserController.java
+++ java_ignite_week_3_spring/src/main/java/tech/obss/controller/UserController.java
@@ -1,6 +1,8 @@
 package tech.obss.controller;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import tech.obss.model.SaveUserRequestDTO;
 import tech.obss.model.UpdateUserRequestDTO;
@@ -8,6 +10,9 @@ import tech.obss.model.UpdateUserRequestDTO;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("")
     public String getUsers() {
         return "Get users";
@@ -20,6 +25,7 @@ public class UserController {
 
     @PostMapping("")
     public SaveUserRequestDTO saveUser(@Valid @RequestBody SaveUserRequestDTO saveUserRequestDTO) {
+        LOGGER.info("Save user request: {}", saveUserRequestDTO);
         return saveUserRequestDTO;
     }
 
