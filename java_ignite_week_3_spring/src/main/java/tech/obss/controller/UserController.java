1cc93fcede88f684cd0876b4598b951f70ab83e2
nazmigorkem <nazmigorkem@hotmail.com>
2023-07-31 16:11:39 +0300

    added exception handler

--- java_ignite_week_3_spring/src/main/java/tech/obss/controller/UserController.java
+++ java_ignite_week_3_spring/src/main/java/tech/obss/controller/UserController.java
@@ -3,6 +3,7 @@ package tech.obss.controller;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.obss.model.SaveUserRequestDTO;
 import tech.obss.model.UpdateUserRequestDTO;
@@ -14,13 +15,13 @@ public class UserController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("")
-    public String getUsers() {
-        return "Get users";
+    public ResponseEntity<String> getUsers() {
+        return ResponseEntity.ok("Get users");
     }
 
     @GetMapping("/{id}")
-    public String getUsers(@PathVariable("id") long id) {
-        return "Get user: " + id;
+    public ResponseEntity<String> getUsers(@PathVariable("id") long id) {
+        return ResponseEntity.ok("Get user: " + id);
     }
 
     @PostMapping("")
@@ -30,8 +31,8 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
-        return "Delete user: " + id;
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
+        return ResponseEntity.ok("Delete user: " + id);
     }
 
     @PutMapping("/{id}")
---
+++ java_ignite_week_3_spring/src/main/java/tech/obss/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package tech.obss.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException e) {
+        LOGGER.error("Runtime exception occurred: {}", e.getMessage());
+        var map = Map.of("message", "Unexpected error occurred.");
+        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        LOGGER.warn("Validation exception occurred");
+        var map = Map.of("message", "Validation exception occurred.");
+        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+    }
+
+}
