a56fc7e19b827ef2d16df9e362a4db6ec6fc0876
nazmigorkem <nazmigorkem@hotmail.com>
2023-07-31 11:19:16 +0300

    initialized spring project

---
+++ java_ignite_week_3_spring/src/main/java/tech/obss/JavaIgniteWeek3SpringApplication.java
@@ -0,0 +1,16 @@
+package tech.obss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+public class JavaIgniteWeek3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaIgniteWeek3SpringApplication.class, args);
+    }
+
+}
---
+++ java_ignite_week_3_spring/src/test/java/tech/obss/JavaIgniteWeek3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.obss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIgniteWeek3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
