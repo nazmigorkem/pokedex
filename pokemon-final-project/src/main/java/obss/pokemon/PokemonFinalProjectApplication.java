9dead255f16646d7bf28d73f25eb6702caa79135
nazmigorkem <nazmigorkem@hotmail.com>
2023-08-03 15:18:33 +0300

    initialized final project

---
+++ pokemon-final-project/src/main/java/obss/pokemon/PokemonFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package obss.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonFinalProjectApplication.class, args);
+    }
+
+}
---
+++ pokemon-final-project/src/test/java/obss/pokemon/PokemonFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package obss.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
