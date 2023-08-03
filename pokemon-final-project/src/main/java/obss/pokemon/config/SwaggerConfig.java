ca1cdaef684ad300f4ba91013a9ff016602027d5
nazmigorkem <nazmigorkem@hotmail.com>
2023-08-03 19:06:51 +0300

    added swagger ui

--- pokemon-final-project/src/main/java/obss/pokemon/config/SwaggerConfig.java
+++ pokemon-final-project/src/main/java/obss/pokemon/config/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package obss.pokemon.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    
+
+    @Bean
+    OpenAPI openAPI() {
+        return new OpenAPI();
+    }
 }
